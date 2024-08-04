1caa585818f07aecf8c77123af159f2fb1c16b81
AlexKeyMSI <koba.ayu@edu.spbstu.ru>
2024-08-05 02:31:38 +0300

    final pdf

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
 document.getElementById("downloadPDF").addEventListener("click", function () {
   var element = document.body;
   html2pdf(element, {
-    margin: 0.5,
+    margin: [10, 10, 10, 10], // Параметры отступов в мм (по желанию можно изменить)
     filename: "CV_Alex_Koba.pdf",
     image: { type: "jpeg", quality: 0.98 },
     html2canvas: { scale: 2, logging: true, dpi: 192, letterRendering: true },
