5c3ee07a25f33314083e2518f4ff169df4ace3e8
AlexKeyMSI <koba.ayu@edu.spbstu.ru>
2024-08-05 01:04:56 +0300

    исправил pdf2

--- script.js
+++ script.js
@@ -15,11 +15,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
 document.getElementById("downloadPDF").addEventListener("click", function () {
   var element = document.body;
-  html2pdf(element, {
-    margin: 0,
+  var opt = {
+    margin: [0.5, 0.5, 0.5, 0.5], // Устанавливаем поля
     filename: "CV_Alex_Koba.pdf",
     image: { type: "jpeg", quality: 0.98 },
-    html2canvas: { scale: 1, logging: true, dpi: 192, letterRendering: true },
-    jsPDF: { unit: "in", format: "letter", orientation: "portrait" },
-  });
+    html2canvas: { scale: 2, logging: true, dpi: 192, letterRendering: true },
+    jsPDF: { unit: "in", format: "a4", orientation: "portrait" }, // Изменяем формат на A4
+    pagebreak: { mode: ["avoid-all"] }, // Избегаем разрыва секций
+  };
+
+  html2pdf().set(opt).from(element).save();
 });
